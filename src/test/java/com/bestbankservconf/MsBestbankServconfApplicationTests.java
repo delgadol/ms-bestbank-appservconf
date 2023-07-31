a54aacee8705f4a6c59810a0aeea513e84b290f3
Luis Delgado <37703993+delgadol@users.noreply.github.com>
2023-07-30 19:26:56 -0500

    Actuliazacion
    Actuliazacion

--- src/test/java/com/bestbankservconf/MsBestbankServconfApplicationTests.java
+++ src/test/java/com/bestbankservconf/MsBestbankServconfApplicationTests.java
@@ -1,13 +1,17 @@
 package com.bestbankservconf;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {com.bestbanks.servconf.MsBestbankServconfApplication.class})
 class MsBestbankServconfApplicationTests {
 
 	@Test
+	@Disabled("Test de Prueba")
 	void contextLoads() {
+	  Assertions.fail();
 	}
 
 }
